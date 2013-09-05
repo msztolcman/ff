b7f85623e6b17d5af4edfc9c99e482bf03170828
Marcin Sztolcman <marcin@urzenia.net>
2013-09-05 11:13:14 +0200

    fixes in help

--- ff.py
+++ ff.py
@@ -180,27 +180,27 @@ def main():
         sys.exit(1)
 
     if config.help:
-        print('''%s pattern
-        [-0|--print0] split results by binary zero instead of new line (useful to work with xargs)
-        [-i|--ignorecase]
-        *[-s|--source source] - optional, see: pattern below
-        *[-p|--pattern]
-        [-g|--regexp] - treat pattern as regular expression (uses Python regexp engine)
-        [-l|--regex-multiline]
-        [-d|--regex-dotall]
-        [-B|--begin] - match pattern to begin of item name (ignored in regexp mode)
-        [-E|--end] - match pattern to end of item name (ignored in regexp mode)
-        [-v|--invert-match]
-        [-m|--mode] - one of: 'all' (default), 'dirs', 'files'
-        [-x|--exec] - execute some command on every found item. In command, placeholders: {path}, {dirname}, {basename} are replaced with correct value
-        [--prefix=PREFIX] - add prefix 'd: ' (directory) or 'f: ' (file) to every found item
-        [--no-display] - don't display element (useful with --exec argument)
-        [--verbose-exec] - show command before execute it
-        [--interactive-exec] - ask before execute command on every item
-        [--vcs] - do not skip VCS directories (.git, .svn etc)
-        [-h|--help]
-        pattern - pattern to search
-        [source1 .. sourceN] - optional source (if missing, use current directory)''' % os.path.basename(sys.argv[0]))
+        print('''%s
+    [-0|--print0] split results by binary zero instead of new line (useful to work with xargs)
+    [-i|--ignorecase]
+    *[-s|--source source] - optional, see: pattern below
+    *[-p|--pattern]
+    [-g|--regexp] - treat pattern as regular expression (uses Python regexp engine)
+    [-l|--regex-multiline]
+    [-d|--regex-dotall]
+    [-B|--begin] - match pattern to begin of item name (ignored in regexp mode)
+    [-E|--end] - match pattern to end of item name (ignored in regexp mode)
+    [-v|--invert-match]
+    [-m|--mode] - one of: 'all' (default), 'dirs', 'files'
+    [-x|--exec] - execute some command on every found item. In command, placeholders: {path}, {dirname}, {basename} are replaced with correct value
+    [--prefix=PREFIX] - add prefix 'd: ' (directory) or 'f: ' (file) to every found item
+    [--no-display] - don't display element (useful with --exec argument)
+    [--verbose-exec] - show command before execute it
+    [--interactive-exec] - ask before execute command on every item
+    [--vcs] - do not skip VCS directories (.git, .svn etc)
+    [-h|--help]
+    pattern - pattern to search
+    [source1 .. sourceN] - optional source (if missing, use current directory)''' % os.path.basename(sys.argv[0]))
         sys.exit()
 
     config.pattern = prepare_pattern(config)
