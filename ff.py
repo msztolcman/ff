b8f616a9453f2959f7a48c46c96eae5347f997fb
Marcin Sztolcman <marcin@urzenia.net>
2013-09-21 07:54:14 +0200

    info about program

--- ff.py
+++ ff.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+""" ff - Easily search and process files.
+    http://mysz.github.io/ff
+    Author: Marcin Sztolcman (marcin@urzenia.net)
+
+    Get help with: ff --help
+    Information about version: ff --version
+"""
+
 from __future__ import print_function, unicode_literals
 
 import argparse
