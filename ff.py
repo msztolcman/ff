3e08d3d97de2c0bb5f7063c2a53774f129c70178
Marcin Sztolcman <marcin@urzenia.net>
2013-09-22 16:28:23 +0200

    small docs improvement for --help-test-plugins arg

--- ff.py
+++ ff.py
@@ -446,7 +446,7 @@ def parse_input_args(args):
     p.add_argument('--plugins-path', type=str,
                    help='additional path where to search plugins (see annotations below)')
     p.add_argument('--version', action='version', version="%s %s\n%s" % (os.path.basename(sys.argv[0]), __version__, args_description))
-    p.add_argument('--help-test-plugins', metavar='TEST_NAME', nargs='?', action='append', default=[],
+    p.add_argument('--help-test-plugins', metavar='TEST_NAME[,TEST2_NAME]', nargs='?', action='append', default=[],
                    help='display help for installed test plugins')
     p.add_argument('anon_pattern', metavar='pattern', type=str, nargs='?',
                    help='pattern to search')
