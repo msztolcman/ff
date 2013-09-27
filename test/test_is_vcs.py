99610c91a93400e20d5774a2b91f3a9d28c3e9be
Marcin Sztolcman <marcin@urzenia.net>
2013-09-27 15:04:29 +0200

    tests for _is_vcs

---
+++ test/test_is_vcs.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, unicode_literals, absolute_import
+
+from pprint import pprint, pformat
+
+from test_manager import *
+
+unittest = import_unittest()
+
+import ff
+
+class TestIsVcs(unittest.TestCase):
+    def test_correct(self):
+        for item in ff._IS_VCS__NAMES.keys():
+            result = ff._is_vcs(item)
+            self.assertTrue(result)
+
+    def test_incorrect(self):
+        for item in ('a', 'sada', 'faewwf3', 32, '', None, True, False):
+            result = ff._is_vcs(item)
+            self.assertFalse(result)
+
+    def test_correct_with_strange_case(self):
+        for item in ff._IS_VCS__NAMES.keys():
+            result = ff._is_vcs(item.swapcase())
+            self.assertFalse(result)
+
+if __name__ == '__main__':
+    unittest.main()
