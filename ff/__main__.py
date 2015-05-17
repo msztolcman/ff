109f764cbee5265b510353951f281045474afe1a
Marcin Sztolcman <marcin@urzenia.net>
2015-05-17 13:22:10 +0200

    cleanups between cli to main

--- ff/__main__.py
+++ ff/__main__.py
@@ -1,9 +1,38 @@
 #!/usr/bin/env python
 #  -*- coding: utf-8 -*-
 
+""" ff - Easily search and process files.
+    http://mysz.github.io/ff
+    Author: Marcin Sztolcman (marcin@urzenia.net)
+
+    Get help with: ff --help
+    Information about version: ff --version
+"""
+
 from __future__ import print_function, unicode_literals, division
 
-from ff.cli import main
+import argparse
+import sys
+
+from ff.cli import parse_input_args
+from ff.processing import process_source
+from ff.utils import disp
+
+def main():
+    """ Run program
+    """
+    try:
+        config = parse_input_args(sys.argv[1:])
+    except argparse.ArgumentError as ex:
+        disp(ex, file=sys.stderr)
+        sys.exit(1)
+
+    try:
+        for source in config.source:
+            process_source(source, config)
+    except KeyboardInterrupt:
+        disp('Interrupted by CTRL-C, aborting', file=sys.stderr)
+
 
 if __name__ == '__main__':
     main()
--- ff/cli.py
+++ ff/cli.py
@@ -1,13 +1,5 @@
 # -*- coding: utf-8 -*-
 
-""" ff - Easily search and process files.
-    http://mysz.github.io/ff
-    Author: Marcin Sztolcman (marcin@urzenia.net)
-
-    Get help with: ff --help
-    Information about version: ff --version
-"""
-
 from __future__ import print_function, unicode_literals, division
 
 import argparse
@@ -20,7 +12,6 @@ import unicodedata
 
 from ff.pattern import prepare_pattern
 from ff.plugin import FFPlugins, FFPlugin
-from ff.processing import process_source
 from ff.utils import disp, u
 
 
@@ -226,19 +217,3 @@ def parse_input_args(args):  ## pylint: disable-msg=too-many-branches, too-many-
         args.delim = "\n"
 
     return args
-
-
-def main():
-    """ Run program
-    """
-    try:
-        config = parse_input_args(sys.argv[1:])
-    except argparse.ArgumentError as ex:
-        disp(ex, file=sys.stderr)
-        sys.exit(1)
-
-    try:
-        for source in config.source:
-            process_source(source, config)
-    except KeyboardInterrupt:
-        disp('Interrupted by CTRL-C, aborting', file=sys.stderr)
