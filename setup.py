ac83ca09fee238180a937f3be698bf90605ff45a
Marcin Sztolcman <marcin@urzenia.net>
2015-06-04 00:29:50 +0200

    use README.rst for pypi

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ import os.path
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-with open(os.path.join(BASE_DIR, 'README.md'), encoding='utf-8') as f:
+with open(os.path.join(BASE_DIR, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
